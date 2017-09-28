73336ff6a8c7001666df060cd2300d1d86533c86
bwagner <bradford.wagner@blackrock.com>
2017-09-28 17:57:41 -0400

    Code cleanup

--- src/components/Search.js
+++ src/components/Search.js
@@ -31,22 +31,14 @@ class Search extends React.Component {
                 <div className="search-books-bar">
                     <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                         <input type="text" value={this.state.query} onChange={this.handleChange} placeholder="Search by title or author"/>
-                        {/*<pre>query: {this.state.query}</pre>*/}
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
                         {this.state.books.map(book => (
-                            <Book key={book.id} book={book} bookshelves={this.props.bookshelves} moveBookToList={(book, listAction) => this.props.moveBookToList(book, listAction)}/>
+                            <Book key={book.id} book={book} bookshelves={this.props.bookshelves}
+                                  moveBookToList={(book, listAction) => this.props.moveBookToList(book, listAction)}/>
                         ))}
                     </ol>
                 </div>
